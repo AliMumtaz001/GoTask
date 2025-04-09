2896e67b5211aba7bd6ac43bf87d612b52b4eb37
HadairAli <alimumtazraza@gmail.com>
2025-04-09 12:47:14 +0500

    Multiple func added

--- main.go
+++ main.go
@@ -1,104 +1,101 @@
 package main
-
 import (
-	"time"
 	"fmt"
 	"log"
 	"os"
-	"sync"
+	"time"
 )
 func main() {
 	start := time.Now() //for executionn time
-	time.Sleep(time.Second * 2)
 
-	var wg sync.WaitGroup //Now you initialized one WaitGroup (wg)
-    Chans := make(chan int,10)
-	
 	content, error := os.ReadFile("Testfile.txt") // to Read file
-	if error != nil {	// if err then log  
+	if error != nil {                             // if err then log
 		log.Fatal(error)
 	}
-	str := string(content) //coonvert it into string
-	wg.Add(10)			//creates 10 Channels (Chans)
-	go func ()  {
-		Chans <- len(str)  //value Stre in chanel
-		wg.Done()	//job is ok
-	}()
-	TotalChar := <- Chans //then again value recieve from Chans and same this process for remaining 9 chans
-	go func ()  {
-		Chans <- WordCount(str)
-		wg.Done()
-	}()
-	Totalword := <- Chans
-	go func ()  {
-		Chans <- LineCount(str)
-		wg.Done()
-	}()
-	TotalLine := <- Chans
-	go func ()  {
-		Chans <- ParaCount(str)
-		wg.Done()
-	}()
-	TotalPara := <- Chans
-	go func ()  {
-		Chans <- ConsinentCount(str)
-		wg.Done()
-	}()
-	TotalCons := <- Chans
-	go func ()  {
-		Chans <- VowelCount(str)
-		wg.Done()
-	}()
-	TotalVowel := <- Chans
-	go func ()  {
-		Chans <- SpaceCount(str)
-		wg.Done()
-	}()
-	TotalSpace := <- Chans
-	go func ()  {
-		Chans <- DigitCount(str)
-		wg.Done()
-	}()
-	TotalDigit := <- Chans
-	go func ()  {
-		Chans <- PuncCount(str)
-		wg.Done()
-	}()
-	TotalPun := <- Chans
-	go func ()  {
-		Chans <- SpecialCount(str)
-		wg.Done()
-	}()
-	TotalSpec := <- Chans
-	go func () {
-		wg.Wait()
-		close(Chans)
-	}()
-	// arr :=[10]string{"Total Characters are :","Total Words are :","Total Lines are :","Total Paragraphs are :",
-	// 				"Total Consinent are : ","Total Vowels are : ","Total Space are : ","Total Digits are :",
-	// 				"Total Punctuation are : ", "Total Special Characters are : "}
-	// for a := range Chans{
-	// 	for i:=0;i<9;i++{
-	// 		final:=arr[i]
-	// 		fmt.Println(final,a)
-	// 	}	
-	// }
-		
-	fmt.Println("Total Characters:  ", TotalChar)
-	fmt.Println("Total Words", Totalword)
-	fmt.Println("Total Lines", TotalLine)
-	fmt.Println("Total Paragraphs are", TotalPara)
-	fmt.Println("Total Consinent are", TotalCons)
-	fmt.Println("Total Vowels are", TotalVowel)
-	fmt.Println("Total Space are", TotalSpace)
-	fmt.Println("Total Digits are", TotalDigit)
-	fmt.Println("Total Punctuation", TotalPun)
-	fmt.Println("Total Special Characters are", TotalSpec)
-	
-	elapse :=time.Since(start)
-	fmt.Printf("page took %s \n",elapse)
-}
 
+	str := string(content) //coonvert it into string
+
+
+	// fmt.Println("Total Characters:  ", len(str))
+	// fmt.Println("Total Words", WordCount(str))
+	// fmt.Println("Total Lines", LineCount(str))
+	// fmt.Println("Total Paragraphs are", ParaCount(str))
+	// fmt.Println("Total Consinent are", ConsinentCount(str))
+	// fmt.Println("Total Vowels are", VowelCount(str))
+	// fmt.Println("Total Space are", SpaceCount(str))
+	// fmt.Println("Total Digits are", DigitCount(str))
+	// fmt.Println("Total Punctuation", PuncCount(str))
+	// fmt.Println("Total Special Characters are", SpaceCount(str))
+	// elapse := time.Since(start)
+	// fmt.Printf("page took %s \n", elapse)
+
+	
+	fmt.Println("Total Characters:  ", len(str))
+	fmt.Println("Total Characters:  ", combinefunc(str))
+	elapse := time.Since(start)
+	fmt.Printf("page took %s \n", elapse)
+}
+type multiples struct {
+	word, line, paragraph, consient, vowel, space, digit, punctuation, Special int
+}
+func combinefunc(s string) multiples {
+	m := multiples{}
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' ' || s[i] == '\n' {
+			m.word++
+		}
+		if s[i] == ' ' {
+			m.space++
+		}
+		if s[i] == '.' {
+			m.line++
+		}
+		if s[i] == '\n' {
+			m.paragraph++
+		}
+		if s[i] == 'b' || s[i] == 'c' || s[i] == 'd' || s[i] == 'f' || s[i] == 'g' ||
+			s[i] == 'h' || s[i] == 'j' || s[i] == 'k' || s[i] == 'l' || s[i] == 'm' ||
+			s[i] == 'n' || s[i] == 'p' || s[i] == 'q' || s[i] == 'r' || s[i] == 's' ||
+			s[i] == 't' || s[i] == 'v' || s[i] == 'w' || s[i] == 'x' || s[i] == 'y' ||
+			s[i] == 'z' || s[i] == 'B' || s[i] == 'C' || s[i] == 'D' || s[i] == 'F' ||
+			s[i] == 'G' || s[i] == 'H' || s[i] == 'J' || s[i] == 'K' || s[i] == 'L' ||
+			s[i] == 'M' || s[i] == 'N' || s[i] == 'P' || s[i] == 'Q' || s[i] == 'R' ||
+			s[i] == 'S' || s[i] == 'T' || s[i] == 'V' || s[i] == 'W' || s[i] == 'X' ||
+			s[i] == 'Y' || s[i] == 'Z' {
+			m.consient++
+		}
+		if s[i] == '!' || s[i] == '?' || s[i] == ',' || s[i] == '.' {
+			m.punctuation += 1
+		}
+		if s[i] == '%' || s[i] == '@' || s[i] == '#' || s[i] == '$' || s[i] == '^' || s[i] == '&' ||
+			s[i] == '*' || s[i] == '(' || s[i] == ')' || s[i] == '_' || s[i] == '+' || s[i] == '-' ||
+			s[i] == '=' || s[i] == '{' || s[i] == '}' || s[i] == '[' || s[i] == ']' || s[i] == '|' ||
+			s[i] == '\\' || s[i] == ':' || s[i] == ';' || s[i] == '"' || s[i] == '\'' || s[i] == '<' ||
+			s[i] == '>' || s[i] == '/' || s[i] == '~' || s[i] == '`' {
+			m.Special += 1
+		}
+		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+			s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' {
+			m.vowel += 1
+		}
+		if s[i] == '0' || s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' ||
+			s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9' {
+			m.digit += 1
+		}
+	}
+	// fmt.Println("Total Characters:  ", len(str))
+	fmt.Println("Total Words", m.word)
+	fmt.Println("Total Lines", m.line)
+	fmt.Println("Total Paragraphs are", m.paragraph)
+	fmt.Println("Total Consinent are", m.consient)
+	fmt.Println("Total Vowels are", m.vowel)
+	fmt.Println("Total Space are", m.space)
+	fmt.Println("Total Digits are", m.digit)
+	fmt.Println("Total Punctuation", m.punctuation)
+	fmt.Println("Total Special Characters are", m.Special)
+	return m
+}
+//function for Words count
 func WordCount(s string) int {
 	word := 0
 	for i := 0; i < len(s); i++ {
@@ -106,41 +103,44 @@ func WordCount(s string) int {
 			word += 1
 		}
 	}
-	return word + 1
-
+	return word
 }
+//function for space count
 func SpaceCount(s string) int {
-	word := 0
+	space := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == ' ' {
-			word += 1
+			space += 1
 		}
 	}
-	return word + 1
+	return space 
 
 }
+//function for line count
 func LineCount(s string) int {
-	word := 0
+	line := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == '.' {
-			word += 1
+			line += 1
 		}
 	}
-	return word + 1
+	return line
 
 }
+//function for Paragraph count
 func ParaCount(s string) int {
-	word := 0
+	paragraph := 0
 	for i := 0; i < len(s); i++ {
-		if  s[i] == '\n'{ 
-				word += 1
+		if s[i] == '\n' {
+			paragraph += 1
 		}
 	}
-	return word + 1
+	return paragraph + 1
 
 }
+//function for Consient count
 func ConsinentCount(s string) int {
-	word := 0
+	cons := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == 'b' || s[i] == 'c' || s[i] == 'd' || s[i] == 'f' || s[i] == 'g' ||
 			s[i] == 'h' || s[i] == 'j' || s[i] == 'k' || s[i] == 'l' || s[i] == 'm' ||
@@ -151,66 +151,56 @@ func ConsinentCount(s string) int {
 			s[i] == 'N' || s[i] == 'P' || s[i] == 'Q' || s[i] == 'R' || s[i] == 'S' ||
 			s[i] == 'T' || s[i] == 'V' || s[i] == 'W' || s[i] == 'X' || s[i] == 'Y' ||
 			s[i] == 'Z' {
-				word++
+			cons++
 		}
 	}
-	return word + 1
-
+	return cons
 }
-
+//function for Punctuation count
 func PuncCount(s string) int {
-	word := 0
+	punct := 0
 	for i := 0; i < len(s); i++ {
-		if   s[i] == '!' || s[i] == '?' || s[i] == ',' || s[i] == '.' {
-			word += 1
+		if s[i] == '!' || s[i] == '?' || s[i] == ',' || s[i] == '.' {
+			punct += 1
 		}
 	}
-	return word + 1
+	return punct
 
 }
-
+//function for Special Char count
 func SpecialCount(s string) int {
-	word := 0
+	special := 0
 	for i := 0; i < len(s); i++ {
-		if s[i] == '%' || s[i] == '@' || s[i] == '#' || s[i] == '$' || s[i] == '^' || s[i] == '&' || 
-		 s[i] == '*' || s[i] == '(' || s[i] == ')' || s[i] == '_' || s[i] == '+' || s[i] == '-' || 
-		 s[i] == '=' || s[i] == '{' || s[i] == '}'|| s[i] == '[' || s[i] == ']' || s[i] == '|' || 
-		 s[i] == '\\' || s[i] == ':' || s[i] == ';' || s[i] == '"' || s[i] == '\'' || s[i] == '<' || 
-		 s[i] == '>'   || s[i] == '/' || s[i] == '~' || s[i] == '`'  {
-			word += 1
+		if s[i] == '%' || s[i] == '@' || s[i] == '#' || s[i] == '$' || s[i] == '^' || s[i] == '&' ||
+			s[i] == '*' || s[i] == '(' || s[i] == ')' || s[i] == '_' || s[i] == '+' || s[i] == '-' ||
+			s[i] == '=' || s[i] == '{' || s[i] == '}' || s[i] == '[' || s[i] == ']' || s[i] == '|' ||
+			s[i] == '\\' || s[i] == ':' || s[i] == ';' || s[i] == '"' || s[i] == '\'' || s[i] == '<' ||
+			s[i] == '>' || s[i] == '/' || s[i] == '~' || s[i] == '`' {
+			special += 1
 		}
-	} //# $ % ^ & * ( ) _ + - = { } [ ] | \ : ; " ' < > , . ? / ~ `
-	return word + 1
+	}
+	return special
 
 }
-
+//function for Vowels count
 func VowelCount(s string) int {
-	word := 0
+	vowel := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
-		 s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' {
-			word += 1
+			s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' {
+			vowel += 1
 		}
 	}
-	return word + 1
+	return vowel 
 }
+//function for Digits count
 func DigitCount(s string) int {
-	word := 0
+	digit := 0
 	for i := 0; i < len(s); i++ {
-		if s[i] == '0' || s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || 
-		s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9' {
-			word += 1
+		if s[i] == '0' || s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' ||
+			s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9' {
+			digit += 1
 		}
 	}
-	return word + 1
+	return digit 
 }
-
-// func WordCount(s string) map[string]int{
-// 	words := strings.Fields(s)
-// 	m := make(map[string]int)
-// 	for _, words := range words{
-// 		m[words] += 1
-// 	}
-// 	fmt.Println(m)
-// 	return m
-// }
